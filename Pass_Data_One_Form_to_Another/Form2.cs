8c6f540679b6898029110f572f5d270839311ef0
Ausha M S <anusgowda050@gmail.com>
2023-03-23 11:22:37 +0530

    Installing iTextSharp package.

--- Pass_Data_One_Form_to_Another/Form2.cs
+++ Pass_Data_One_Form_to_Another/Form2.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace Pass_Data_One_Form_to_Another
 {
