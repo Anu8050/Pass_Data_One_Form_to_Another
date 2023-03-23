d2d9b427d4cb64a50fc47c465dc8e482454e2e80
Ausha M S <anusgowda050@gmail.com>
2023-03-23 10:38:54 +0530

    Add project files.

---
+++ Pass_Data_One_Form_to_Another/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pass_Data_One_Form_to_Another
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
