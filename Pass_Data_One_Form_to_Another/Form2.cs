80931cf62540fc4aeb597bd9dab9d167242f8932
Ausha M S <anusgowda050@gmail.com>
2023-03-23 11:35:11 +0530

    Adding button to form2 and generate pdf when click this button.

--- Pass_Data_One_Form_to_Another/Form2.cs
+++ Pass_Data_One_Form_to_Another/Form2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -17,6 +18,22 @@ namespace Pass_Data_One_Form_to_Another
             label1.Text = Form1.SetValueForText1;
             label2.Text = Form1.SetValueForText2;
             label3.Text = Form1.SetValueForText3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Document document = new Document();
+            PdfWriter.GetInstance(document, new FileStream("C:\\Users\\User\\Documents\\Anusha.pdf", FileMode.Create));
+            document.Open();
+            label1.Text = Form1.SetValueForText1;
+            label2.Text = Form1.SetValueForText2;
+            label3.Text = Form1.SetValueForText3;
+            String str = label1.Text + "\n" + label2.Text + "\n" + label3.Text;
+
+            Paragraph p = new Paragraph(str);
+            document.Add(p);
+            document.Close();
         }
     }
 }
