0f4e9d34fef6940546b704a108f61369fa1c7bb9
Ausha M S <anusgowda050@gmail.com>
2023-03-24 10:52:46 +0530

    Update form3.

--- Pass_Data_One_Form_to_Another/Form3.cs
+++ Pass_Data_One_Form_to_Another/Form3.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,40 @@ namespace Pass_Data_One_Form_to_Another
         {
             InitializeComponent();
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync(2000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.CancelAsync();
+        }
+
+        public void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker helperBW = sender as BackgroundWorker;
+            int arg = (int)e.Argument;
+            e.Result = BackgroundProcessLogicMethod(helperBW, arg);
+            if( helperBW.CancellationPending)
+            {
+                e.Cancel = true;    
+            }
+        }
+
+        private int BackgroundProcessLogicMethod(BackgroundWorker bw, int a)
+        {
+            int result = 0;
+            Thread.Sleep(20000);
+            MessageBox.Show("I was doing some work in the background.");
+            return result;
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled) MessageBox.Show("Operation was canceled");
+            else if (e.Error != null) MessageBox.Show(e.Error.Message);
+            else MessageBox.Show(e.Result.ToString());
+        }
 }
