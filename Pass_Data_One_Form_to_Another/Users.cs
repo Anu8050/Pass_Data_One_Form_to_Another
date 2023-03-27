888be790ae284b80c32f16db21312405a0f94736
Ausha M S <anusgowda050@gmail.com>
2023-03-27 12:02:17 +0530

    Creating another class called users.

---
+++ Pass_Data_One_Form_to_Another/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_Data_One_Form_to_Another
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
