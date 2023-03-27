939e01176dc1156a4747886e483e9553d23cb3c0
Ausha M S <anusgowda050@gmail.com>
2023-03-27 18:56:31 +0530

    Implementation of HelpProvider.

--- Pass_Data_One_Form_to_Another/Form17.Designer.cs
+++ Pass_Data_One_Form_to_Another/Form17.Designer.cs
@@ -34,6 +34,11 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -78,15 +83,61 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(69, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "HelpProvider example.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // textBox2
+            // 
+            this.helpProvider1.SetHelpString(this.textBox2, "Help provider here");
+            this.textBox2.Location = new System.Drawing.Point(54, 271);
+            this.textBox2.Name = "textBox2";
+            this.helpProvider1.SetShowHelp(this.textBox2, true);
+            this.textBox2.Size = new System.Drawing.Size(136, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.helpProvider1.SetHelpString(this.button2, "Help provider here");
+            this.button2.Location = new System.Drawing.Point(250, 271);
+            this.button2.Name = "button2";
+            this.helpProvider1.SetShowHelp(this.button2, true);
+            this.button2.Size = new System.Drawing.Size(86, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Click";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form17
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form17";
             this.Text = "Form17";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
@@ -102,5 +153,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
     }
 }
