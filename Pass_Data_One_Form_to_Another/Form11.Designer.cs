ed6512f6d31ba8432b3329e97036e8455a7849ed
Ausha M S <anusgowda050@gmail.com>
2023-03-27 11:53:02 +0530

    Adding dummy data to ComboBox.

--- Pass_Data_One_Form_to_Another/Form11.Designer.cs
+++ Pass_Data_One_Form_to_Another/Form11.Designer.cs
@@ -33,12 +33,17 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(162, 98);
+            this.label1.Location = new System.Drawing.Point(166, 100);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 20);
             this.label1.TabIndex = 0;
@@ -66,7 +71,7 @@
             "Kerala",
             "Andhra Pradesh",
             "Maharashtra"});
-            this.comboBox1.Location = new System.Drawing.Point(270, 90);
+            this.comboBox1.Location = new System.Drawing.Point(274, 92);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(210, 28);
             this.comboBox1.TabIndex = 2;
@@ -90,11 +95,63 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "?";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "State";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(296, 272);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(210, 28);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(292, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(165, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(425, 29);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "ComboBox with class and dummy data";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(162, 313);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Selected State";
+            // 
             // Form11
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -115,5 +172,10 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Pass_Data_One_Form_to_Another/Form11.cs
+++ Pass_Data_One_Form_to_Another/Form11.cs
@@ -27,6 +27,25 @@ namespace Pass_Data_One_Form_to_Another
             comboBox1.Items.Add("Manipur");
             comboBox1.Items.Add("Punjab");
             comboBox1.Items.Add("Sikkim");
+            comboBox1.SelectedIndex = 1;
+            comboBox2.Items.Clear();
+            List<State> list = new List<State>();
+            list.Add(new State() { ID = 1, Name = "Manipur" });
+            list.Add(new State() { ID = 2, Name = "Punjab" });
+            list.Add(new State() { ID = 3, Name = "Sikkim" });
+            comboBox2.DataSource = list;
+            comboBox2.ValueMember = "ID";
+            comboBox2.DisplayMember = "Name";
+
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            State obj = comboBox2.SelectedItem as State;
+            if(obj != null) 
+                label6.Text = obj.Name;
+
+            
         }
     }
 }
