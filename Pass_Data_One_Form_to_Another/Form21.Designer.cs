c152b9542c53a0c589c64dcf91f39c665d9b1323
Ausha M S <anusgowda050@gmail.com>
2023-03-28 13:21:46 +0530

    Implementation of maskedtextbox.

--- Pass_Data_One_Form_to_Another/Form21.Designer.cs
+++ Pass_Data_One_Form_to_Another/Form21.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // maskedTextBox1
@@ -42,6 +43,7 @@
             this.maskedTextBox1.TabIndex = 0;
             this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
             this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.maskedTextBox1.TypeValidationCompleted += new System.Windows.Forms.TypeValidationEventHandler(this.maskedTextBox1_TypeValidationCompleted);
             // 
             // button1
             // 
@@ -53,11 +55,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(161, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MaskedTextbox example.";
+            // 
             // Form21
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.maskedTextBox1);
             this.Name = "Form21";
@@ -71,5 +85,6 @@
 
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Pass_Data_One_Form_to_Another/Form21.cs
+++ Pass_Data_One_Form_to_Another/Form21.cs
@@ -19,13 +19,26 @@ namespace Pass_Data_One_Form_to_Another
 
         private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
-
+            MessageBox.Show("Error: " + e.RejectionHint.ToString() + "Position: " + e.Position.ToString());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Date: " + maskedTextBox1.Text);
 
+        }
+
+        private void maskedTextBox1_TypeValidationCompleted(object sender, TypeValidationEventArgs e)
+        {
+            if(e.IsValidInput) 
+            {
+                DateTime dateTime = (DateTime)e.ReturnValue;
+                MessageBox.Show("Validated Date: " + dateTime.ToShortDateString());
+            }
+            else
+            {
+                MessageBox.Show("InValidated Date: " + maskedTextBox1.Text.ToString());
+            }   
         }
     }
 }
