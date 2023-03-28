038037d8c736aa31aa792d1a4c9543acb6223868
Ausha M S <anusgowda050@gmail.com>
2023-03-28 16:37:52 +0530

    Implementation of PerformanceCounter.

--- Pass_Data_One_Form_to_Another/Form23.Designer.cs
+++ Pass_Data_One_Form_to_Another/Form23.Designer.cs
@@ -29,11 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.process1 = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(115, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PerformanceCounter example.";
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // Form23
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form23";
             this.Text = "Form23";
+            this.Load += new System.EventHandler(this.Form23_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Diagnostics.Process process1;
     }
 }
--- Pass_Data_One_Form_to_Another/Form23.cs
+++ Pass_Data_One_Form_to_Another/Form23.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,29 @@ namespace Pass_Data_One_Form_to_Another
 {
     public partial class Form23 : Form
     {
+        private PerformanceCounter cpuCounter;
         public Form23()
         {
             InitializeComponent();
+            InitialiseCPUCounter();
+            timer1.Start();
+        }
+        
+        private void Form23_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void InitialiseCPUCounter()
+        {
+            cpuCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float tmp = cpuCounter.NextValue();
+            textBox1.Text = String.Format("{0} %", tmp);
         }
     }
 }
